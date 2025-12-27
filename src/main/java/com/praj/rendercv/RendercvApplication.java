63971e8ada98cff8dd654e159afcbb99a9c8b146
Prajwal <160503760+Prajwalkadam29@users.noreply.github.com>
2025-12-27 22:23:07 +0530

    Create RendercvApplication class for Spring Boot

---
+++ src/main/java/com/praj/rendercv/RendercvApplication.java
@@ -0,0 +1,13 @@
+package com.praj.rendercv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendercvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendercvApplication.class, args);
+	}
+
+}
